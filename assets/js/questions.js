863907d34e3070369de23e0eea5055868647fc99
Indiara dos Santos Sá Barreto <66640743+indiarasabarreto@users.noreply.github.com>
2022-05-04 05:31:52 -0300

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,63 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Assinale a alternativa que apresenta os nervos cranianos que foram mencionados no caso clínico:",
+        answer: "B) Abducente, facial, trigêmeo, hipoglosso, acessório, vago e glossofaríngeo",
+        options: [
+            "A) Óptico, facial,olfatório, vago, acessório, trigêmeo e abducente",
+            "B) Abducente, facial, trigêmeo, hipoglosso, acessório, vago e glossofaríngeo",
+            "C) Facial, vestíbulococlear, olfatório, acessório, vago e trigêmeo"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quando se deu o início dos sintomas do paciente? Assinale a alternativa correta.",
+        answer: "A) Após esforço físico intenso e uma discussão familiar",
+        options: [
+            "A) Após esforço físico intenso e uma discussão familiar",
+            "B) Após uma discussão familiar",
+            "C) Após fortes dores de cabeça"
+        ]
+    },
+    {
+        numb: 3,
+        question: "O SNP é composto por nervos, sendo eles:",
+        answer: "B) Cranianos (12 pares) que emergem do encéfalo, e os raquidianos ou espinais (31 pares) que emergem a partir da medula espinal.",
+        options: [
+            "A) Cranianos (10 pares) que emergem do encéfalo, e os raquidianos ou espinais (31 pares) que emergem a partir da medula espinal.",
+            "B) Cranianos (12 pares) que emergem do encéfalo, e os raquidianos ou espinais (31 pares) que emergem a partir da medula espinal.",
+            "C) Os raquidianos ou espinais (32 pares) que emergem a partir da medula espinal."
+        ] 
+    },
+    {
+        numb: 4,
+        question: "O nervo vestibular e o nervo coclear é formado por um nervo sensitivo somático. Assinale a alternativa correta:",
+        answer: "B) Nervo vestibulococlear VIII",
+        options: [
+            "A) Nervo oculomotor",
+            "B) Nervo vestibulococlear VIII",
+            "C) Nervo glossofaríngeo"
+        ]
+    },
+    {
+        numb: 5,
+        question: "De acordo com o estudo de caso, qual nervo é puramente sensitivo:",
+        answer: "C) O nervo vestibulococlear",
+        options: [
+            "A) O nervo vargo",
+            "B) O nervo hipoglosso",
+            "C) O nervo vestibulococlear"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Assinale a alternativa incorreta com relação aos nervos:",
+        answer: "C) O nervo acessório trabalha com os músculos da língua e os feixes musculares pela alça cervical.",
+        options: [
+            "A) Os nervos Acessório e o Hipoglosso têm apenas funções motoras.",
+            "B) Os nervos Glossofaríngeo e Vago exercem uma função mista",
+            "C) O nervo acessório trabalha com os músculos da língua e os feixes musculares pela alça cervical."
+        ]
+    }
+];
